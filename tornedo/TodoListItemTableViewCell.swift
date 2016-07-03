61667983bf4b361dbe22a8f1b1ec1d0aa0a0db86
otiai10 <otiai10@gmail.com>
2016-07-03 20:38:32 +0200

    TableView / TableViewCell

---
+++ tornedo/TodoListItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TodoListItemTableViewCell.swift
+//  tornedo
+//
+//  Created by hiromu otiai on 7/3/16.
+//  Copyright © 2016 otiai10.com. All rights reserved.
+//
+
+import UIKit
+
+class TodoListItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
