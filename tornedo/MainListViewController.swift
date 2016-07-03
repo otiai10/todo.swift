cb628453d87339001875ad2b8fd12e2582be4fe4
otiai10 <otiai10@gmail.com>
2016-07-03 21:16:43 +0200

    Implement TableViewDataSource

--- tornedo/MainListViewController.swift
+++ tornedo/MainListViewController.swift
@@ -8,7 +8,10 @@
 
 import UIKit
 
-class MainListViewController: UIViewController {
+class MainListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var mainTableView: UITableView!
+    var list = [];
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +22,14 @@ class MainListViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0;
+    }
 
-
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("TodoListItem")! as UITableViewCell;
+        return cell;
+    }
 }
 
